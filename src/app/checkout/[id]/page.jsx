ab7506733e685b88b0c88c739cf321ff4f450c6a
appoclayptor <shahreza1414@gmail.com>
2025-02-02 13:47:59 +0600

    Add service API route and checkout form; implement service data fetching and booking functionality

---
+++ src/app/checkout/[id]/page.jsx
@@ -0,0 +1,16 @@
+import CheckoutForm from '@/components/forms/CheckoutForm';
+import React from 'react';
+
+const CheckoutPage = async ({ params }) => {
+   const p = await params
+   const res = await fetch(`http://localhost:3000/api/service/${p.id}`);
+   const data = await res.json();
+   
+   return (
+      <div>
+         <CheckoutForm></CheckoutForm>
+      </div>
+   );
+};
+
+export default CheckoutPage;
